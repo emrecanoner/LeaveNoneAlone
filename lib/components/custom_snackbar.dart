911c0566502b09c3f51621704391c2433479e056
Emre Can <emrecanoner@eskisehir.edu.tr>
2022-12-01 17:57:30 +0300

    Changes committed

---
+++ lib/components/custom_snackbar.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:lna/utils/constant.dart';
+
+class CustomSnackBarContent extends StatelessWidget {
+  const CustomSnackBarContent({
+    Key? key,
+    required this.errorMessage,
+  }) : super(key: key);
+
+  final String errorMessage;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      clipBehavior: Clip.none,
+      children: [
+        Container(
+          padding: EdgeInsets.all(16),
+          height: gHeight / 8,
+          decoration: BoxDecoration(
+            color: Color(0xffc72c45),
+            borderRadius: BorderRadius.all(Radius.circular(20)),
+          ),
+          child: Row(
+            children: [
+              SizedBox(width: gWidth / 10),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Oh Snap!',
+                      style: TextStyle(fontSize: 18, color: Colors.white),
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    Spacer(),
+                    Text(
+                      errorMessage,
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        Positioned(
+          bottom: 0,
+          child: ClipRRect(
+            borderRadius:
+                const BorderRadius.only(bottomLeft: Radius.circular(20)),
+            child: SvgPicture.asset(
+              "assets/icons/bubbles.svg",
+              height: gHeight / 15,
+              width: gWidth / 10,
+              color: Color(0xff801336),
+            ),
+          ),
+        ),
+        Positioned(
+            top: -(gHeight / 30),
+            left: 0,
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                SvgPicture.asset(
+                  "assets/icons/fail.svg",
+                  height: gHeight / 20,
+                ),
+                Positioned(
+                  top: gHeight / 20,
+                  child: SvgPicture.asset(
+                    "assets/icons/close.svg",
+                    height: gHeight / 50,
+                  ),
+                )
+              ],
+            )),
+      ],
+    );
+  }
+}
